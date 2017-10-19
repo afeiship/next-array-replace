2d45aeac0d8406b5724ad9a3537c5b23246eda29
afeiship <1290657123@qq.com>
2017-10-19 14:13:14 +0800

    issue; update

--- test/test.js
+++ test/test.js
@@ -15,4 +15,29 @@ describe('next/replace', function () {
     assert.equal( res, 'AAA-bbb-1234 world Fei!');
   });
 
+
+  it('nx.replace', function () {
+    var REPLACE_RE1 = /[A-Z]/g;
+    var REPLACE_RE2 = /-/g;
+    var DATE_SPACE = ' ';
+    var DATE_DASH = '/';
+
+    var arr= [
+      [REPLACE_RE1,DATE_SPACE],
+      [REPLACE_RE2,DATE_DASH]
+    ];
+
+    var str = '2017-10-19T02:10:42.903Z';
+    var res = nx.replace( str, arr);
+
+    console.log(res == '2017/10/19 02:10:42.903');
+
+    //todo: WHY????
+
+    // assert.equal( res, '2017/10/19 02:10:42.903');
+  });
+
+
+
+
 });
