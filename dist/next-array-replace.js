16aac6344a02f730892c8456b1f4c48b38657c76
afeiship <1290657123@qq.com>
2019-06-21 14:27:49 +0800

    refactor: use new build script

--- dist/next-array-replace.js
+++ dist/next-array-replace.js
@@ -1,3 +1,11 @@
+/*!
+ * name: next-array-replace
+ * url: https://github.com/afeiship/next-array-replace
+ * version: 1.0.0
+ * date: 2019-06-21T06:27:40.483Z
+ * license: MIT
+ */
+
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
@@ -14,3 +22,5 @@
     module.exports = nx.arrayReplace;
   }
 })();
+
+//# sourceMappingURL=next-array-replace.js.map
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var nx = require('next-js-core2');
 require('../src/next-array-replace');
 
-describe('next/arrayReplace', function() {
+describe('Basic test', function() {
   test('nx.arrayReplace', function() {
     var arr = [[/a/g, 'A'], ['hello', 'world']];
     var str = 'aaa-bbb-1234 hello Fei!';
@@ -16,14 +16,22 @@ describe('next/arrayReplace', function() {
     var DATE_SPACE = ' ';
     var DATE_DASH = '/';
 
-    var arr = [
-      [REPLACE_RE1, DATE_SPACE],
-      [REPLACE_RE2, DATE_DASH]
-    ];
+    var arr = [[REPLACE_RE1, DATE_SPACE], [REPLACE_RE2, DATE_DASH]];
 
     var str = '2017-10-19T02:10:42.903Z';
     var res = nx.arrayReplace(str, arr);
 
     expect(res).toBe('2017/10/19 02:10:42.903 ');
+  });
+
+  test('has escapd regexp', () => {
+    var str =
+      '<li>这一行 &amp;  &#34; </li>&#35;</ul>↵<!-- /wp:tss/list -->↵↵<!-- wp:paragraph -->↵<p></p>↵<!-- /wp:paragraph -->↵↵<!-- wp:&#35;paragraph -->↵<p>&#34;</p>';
+    var regs = [[new RegExp('&#34;', 'g'), '"'], [new RegExp('&#35;', 'g'), '@']];
+
+    var res = nx.arrayReplace(str, regs);
+    expect(res).toBe(
+      '<li>这一行 &amp;  " </li>@</ul>↵<!-- /wp:tss/list -->↵↵<!-- wp:paragraph -->↵<p></p>↵<!-- /wp:paragraph -->↵↵<!-- wp:@paragraph -->↵<p>"</p>'
+    );
   });
 });
